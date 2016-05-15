10aa66b3812dde4de76c1f334ffd354e9350dcad
trizzle21 <tdross21@gmail.com>
2016-05-15 04:08:22 -0700

    built data parser

--- dataprep.py
+++ dataprep.py
@@ -1,17 +1,20 @@
 #Prepares data for classification
 import datetime as dt
-import panda as pd
+import pandas as pd
 import numpy as np
 
 
-DATE_FORMAT = "%b/%d/%Y %H"
 
-LOG_DATA = "log_data_september.csv"
-MOBILE_INFO_SEPTEMBER = "mobile_info_all_septermber_1st.csv"
-LOG_DATA_COL= ["deviceid", "log_timestamp", "data_all"]
-MOBILE_INFO_COL= ["device_id", "timestamp", "base_station_id"]
 
-def convert_date(date_str, date_format):
+LOG_DATA = "log_data_septermber_1st.csv"
+MOBILE_INFO_SEPTEMBER = "mobile_info_all_september.csv"
+
+LOG_DATA_COL = ["deviceid", "log_timestamp", "data_all"]
+MOBILE_INFO_COL = ["device_id", "timestamp", "base_station_id"]
+BASE_STATION_PRACTICE = 452
+Merged_Group_Col = ["device_id", "data_all", "timestamp", "base_station_id" ]
+
+def parse_date(date_str):
 	""""takes in a date_str and returns a 
 
 	arguments: date_str
@@ -20,29 +23,47 @@ def convert_date(date_str, date_format):
 	example:
 	parse_date("9/1/16 3:10:56", "%b/%d/%Y %H")
 	"9/1/16 3:10"
-
-
-
 	"""
-	newTime = dt.striptime(datetime, date_format)
-	return newTime.toString()
+	newTime = dt.dt.strptime(datetime, "%Y-%m-%d %H:%M:%S")
+	return [newTime.year, newTime.month, newTime.day, newTime.hour]
+
+def createDate(date_str):
+	timeStampArray = parse_date(date_str)
+	newstr =""
+	for i in timeStampArray:
+		newstr = newstr + " " + str(i)
+	return newstr
 
 
 
-
-def convertingDates(pdDataframe, colName):
-	""" Takes in a data frame and applies 
-	Args:
-	pdDateframe: PandaData_frame
-	colName: Will include the name of the time stamp column
-
-	Returns
-	newpdDataFrame with converted date column
+def filterCellTower(pdDataFrame, cell_tower):
+	"""Takes a pdDataFrame and filters celltower
+		args
+			pddataframe
+			cell tower string
+		outputs new dataframe
 	"""
+	newpdDataFrame = pddataframe[pdDataframe['base_station_id'].isin([cell_tower_string])]
+	return newpdDataFrame
+
+
 
 
 
 def main():
 	#read_csv returns a pdDataframe. Whoop de doo
-	Log = pd.read_csv(LOG_DATA, LOG_DATA_COL)
-	Mobile = pd.read_csv(MOBILE_INFO_SEPTEMBER, MOBILE_INFO_COL)
+	mobile = pd.read_csv(MOBILE_INFO_SEPTEMBER, index_col=0, usecols=["device_id", "timestamp", "base_station_id"])
+	log = pd.read_csv(LOG_DATA,index_col=0, usecols=["device_id", "log_timestamp", "data_all"])
+	mobile = filterCellTower(Log, BASE_STATION_PRACTICE)
+	mobile = mobile["timestamp"].apply(createDate)
+	log = log["log_timestamp"].apply(createDate)
+	MergedGroup = mobile.join(log, on = 'log_timestamp')
+
+	MergedGroup.groupby('timestamp').sum()
+
+	return MergedGroup
+
+
+main()
+
+
