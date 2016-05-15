9c14387b8c6acb8ec4b1ef64d3712e7125a56a89
svt125 <Jhardjadinata14@gmail.com>
2016-05-15 12:32:07 -0700

    Initial commit of original, merged data for Sept. 1

--- dataprep.py
+++ dataprep.py
@@ -3,11 +3,8 @@ import datetime
 import pandas as pd
 import numpy as np
 
-
-
-
-LOG_DATA = "log_data_septermber_1st.csv"
-MOBILE_INFO_SEPTEMBER = "mobile_info_all_september.csv"
+LOG_DATA = "log_data_2015_09_01.csv"
+MOBILE_INFO_SEPTEMBER = "mobile_info_all_sept_1.csv"
 
 LOG_DATA_COL = ["deviceid", "log_timestamp", "data_all"]
 MOBILE_INFO_COL = ["device_id", "timestamp", "base_station_id"]
@@ -46,24 +43,13 @@ def filterCellTower(pddp, cell_tower):
 	newlog = pddp[pddp['base_station_id'].isin([452])]
 	return newlog
 
-
-
-
-
 def main():
 	#read_csv returns a pdDataframe. Whoop de doo
-	mobile = pd.read_csv(MOBILE_INFO_SEPTEMBER, index_col=0, usecols=["device_id", "timestamp", "base_station_id"])
-	log = pd.read_csv(LOG_DATA, index_col=0, usecols=["device_id", "log_timestamp", "data_all"])
+	mobile = pd.read_csv(MOBILE_INFO_SEPTEMBER, usecols=["device_id", "base_station_id"])
+	log = pd.read_csv(LOG_DATA, usecols= ["log_timestamp", "device_id", "data_all"])
 	mobile = filterCellTower(mobile, BASE_STATION_PRACTICE)
-	mobile.index = mobile.index.map(createDate)
-	log = log["log_timestamp"].apply(createDate)
-	MergedGroup = mobile.join(log, on ="log_timestamp")
-
-	MergedGroup.groupby('timestamp').sum()
+	MergedGroup = pd.merge(mobile,log,how="left",on=["device_id","device_id"])
 
 	return MergedGroup
-
-
-main()
 
 
