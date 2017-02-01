9f080aca860d35c5b6aaacca0c74ad8d2f5e5220
trizzle21 <tdross21@gmail.com>
2017-01-31 23:52:33 -0800

    adding needed functions

--- Sample/LinearPrediction.py
+++ Sample/LinearPrediction.py
@@ -8,4 +8,14 @@ We will be predicting total usage of data per day.
 """
 
 
-def dailyDataUsage(dataArray):
+def dailyDataUsage(xValues, yValues):
+	# yValues comes in 
+	# uses the trapazoidal rule to calculate data usage over the day
+	if(len(xValues) != len(yValues)):
+		raise ValueError
+	counter,usage = 0,0
+	while counter < len(dataArray):
+		usage += (xValues[counter+1] - xValues[counter])*((yValues[counter] + yValues[counter+1])/2)
+		counter += 2
+	return usage
+	
