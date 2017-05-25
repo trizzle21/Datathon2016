95cba5beb8d487c4946a73637e983d1e04a678d4
trizzle21 <tdross21@gmail.com>
2017-05-25 00:54:14 -0400

    formatting code and reviewing functions

--- Sample/LinearPrediction.py
+++ Sample/LinearPrediction.py
@@ -21,45 +21,40 @@ The prediction will either be positive or negative, and start from the last valu
 
 
 
+
 def dailyDataUsage(xValues, yValues):
-	# 
-	# uses the trapazoidal rule to calculate data usage over the day
-	if(len(xValues) != len(yValues)):
-		raise ValueError
-	counter,usage = 0,0
-	while counter < len(dataArray):
-		usage += (xValues[counter+1] - xValues[counter])*((yValues[counter] + yValues[counter+1])/2)
-		counter += 2
-	return usage
+    #
+    # uses the trapazoidal rule to calculate data usage over the day
+    if len(xValues) != len(yValues):
+        counter, usage = 0, 0
+    while counter < len(dataArray):
+        usage += (xValues[counter+1] - xValues[counter])*((yValues[counter] + yValues[counter+1])/2)
+        counter += 2
+    return usage
 
 
 
 
 #Build TestData time series
 def buildTrainingData(trainingData):
-	timeSeries = {}
-	day = 0 
-	for i in trainingData:
-		timeSeries[day] = dailyDataUsage(i[0],i[1]);
-	return timeSeries	
+    time_series = {}
+    day = 0
+    for i in trainingData:
+        time_series[day] = dailyDataUsage(i[0], i[1])
+    return time_series
 
- 
+
 def incrementSeries(dailyUsage):
-	r = {0: dailyUsage.pop(0)}
-	for key, value in timeSeries.iteritems():
-		r[key] = value - r[key-1]
-	return r
+    r = {0: dailyUsage.pop(0)}
+    for key, value in timeSeries.iteritems():
+        r[key] = value - r[key-1]
+    return r
 
- def ARcoefficient(incrementSeriesData):
-	# 
+def ARcoefficient(incrementSeriesData):
 	#
 	#
-	g, k = [],1
-	while k < len(dailyUsage):
-		dailyUsage[k] + dailyUsage[k-1] - dailyUsage[k]
-		g.append()
-
-
-
-
-#Predictive Equation 
+	#
+    g, k = [],1
+    while k < len(dailyUsage):
+        dailyUsage[k] + dailyUsage[k-1] - dailyUsage[k]
+        g.append()
--- Sample/dataprep.py
+++ Sample/dataprep.py
@@ -22,52 +22,54 @@ MOBILE_INFO_SEPT_1ST = os.path.join(dir_name, 'Data', "mobile_info_all_sept_1" +
 LOG_DATA_COL = ["deviceid", "log_timestamp", "data_all"]
 MOBILE_INFO_COL = ["device_id", "timestamp", "base_station_id"]
 
-#in case one wasn't selected. returns 
-try: 
-	BASE_STATION_PRACTICE = sys.argv[1]
+#in case one wasn't selected. returns
+try:
+    BASE_STATION_PRACTICE = sys.argv[1]
 except:
-	BASE_STATION_PRACTICE = 452
+    BASE_STATION_PRACTICE = 452
 
 Merged_Group_Col = ["device_id", "data_all", "timestamp", "base_station_id" ]
 
 def parse_date(date_str):
-	""""takes in a date_str and returns a 
+    """takes in a date_str and returns a
 
 	arguments: date_str
 	outputs: time formated in date_format
 
 	example:
 	parse_date("9-1-16 3:10:56", "%b/%d/%Y %H")
-	"9/1/16 3:10"
+	9/1/16 3:10"
 	"""
-	newTime = datetime.datetime.strptime(date_str, "%m/%d/%Y %H:%M")
-	return newTime.strftime("%y-%m-%d %H:%M")
+    new_time = datetime.datetime.strptime(date_str, "%m/%d/%Y %H:%M")
+    return new_time.strftime("%y-%m-%d %H:%M")
 
 
 	#[newTime.year, newTime.month, newTime.day, newTime.hour, newTime.minute]
 	#%Y-%m-%d %H:%M:%S
 
 
-def createDate(date_str):
-	timeStampArray = parse_date(date_str)
-	newstr = ""
-	newstr = str(timeStampArray[1]) +'-' + str(timeStampArray[2]) + '-' + str(timeStampArray[0]) + ' ' + str(timeStampArray[3]) + ':' + str(timeStampArray[4])
-	return newstr
+def create_date(date_str):
+    time_stamp_array = parse_date(date_str)
+    newstr = ""
+    newstr = str(time_stamp_array[1]) +'-' + str(time_stamp_array[2]) + '-' \
+    + str(time_stamp_array[0]) + ' ' + str(time_stamp_array[3]) + ':' + str(time_stamp_array[4])
+    return newstr
 
 
 
-def dateFilter(date_str):
-	return date_str
+def date_filter(date_str):
+    """ TODO"""
+    return date_str
 
-def filterCellTower(pddp, cell_tower):
-	"""Takes a pdDataFrame and filters celltower
+def filter_cell_tower(pddp, cell_tower):
+    """Takes a pdDataFrame and filters celltower
 		args
 			pddataframe
 			cell tower string
 		outputs new dataframe
 	"""
-	newlog = pddp[pddp['base_station_id'].isin([BASE_STATION_PRACTICE])]
-	return newlog
+    newlog = pddp[pddp['base_station_id'].isin([BASE_STATION_PRACTICE])]
+    return newlog
 
 
 def print_full(x):
@@ -88,57 +90,57 @@ def print_full(x):
 
 def main():
 	#read_csv returns a pdDataframe.
-	mobile = pd.read_csv(MOBILE_INFO_SEPT_1ST, usecols=["device_id", "base_station_id"])
-	log = pd.read_csv(LOG_DATA, usecols=["log_timestamp", "device_id", "data_all"])
-	mobile = mobile[mobile['base_station_id'].notnull()]
-	mobile = filterCellTower(mobile, BASE_STATION_PRACTICE)
+    mobile = pd.read_csv(MOBILE_INFO_SEPT_1ST, usecols=["device_id", "base_station_id"])
+    log = pd.read_csv(LOG_DATA, usecols=["log_timestamp", "device_id", "data_all"])
+    mobile = mobile[mobile['base_station_id'].notnull()]
+    mobile = filterCellTower(mobile, BASE_STATION_PRACTICE)
 
 	#Filter the data
 	#mobile.index = mobile.index.map(createDate)
 	#mobile["timestamp"] = mobile["timestamp"].apply(dateFilter)
 	#log["log_timestamp"] = log["log_timestamp"].apply(dateFilter)
 	#log.columns= ["device_id", "timestamp", "data_all"]
-	MergedGroup =  pd.merge(log, mobile, how="left", on=['device_id','device_id'])
-	MergedGroup = MergedGroup[MergedGroup['base_station_id'].notnull()]
+    MergedGroup = pd.merge(log, mobile, how="left", on=['device_id','device_id'])
+    MergedGroup = MergedGroup[MergedGroup['base_station_id'].notnull()]
 
 	#Set to index by timestamp, then group by the hour and sum data_all.
-	MergedGroup['log_timestamp'] = pd.to_datetime(MergedGroup['log_timestamp'])
-	MergedGroup = MergedGroup.set_index('log_timestamp')
-	return [np.unique(MergedGroup.index.map(lambda t: t.hour)), MergedGroup['data_all'].groupby(MergedGroup.index.map(lambda t: t.hour)).sum().values]
+    MergedGroup['log_timestamp'] = pd.to_datetime(MergedGroup['log_timestamp'])
+    MergedGroup = MergedGroup.set_index('log_timestamp')
+    return [np.unique(MergedGroup.index.map(lambda t: t.hour)), MergedGroup['data_all'].groupby(MergedGroup.index.map(lambda t: t.hour)).sum().values]
 
 
 
 def lagrange(xdata, ydata):
-	l = []
-	for i in range(0,24):
-		l.append(lg.lagrange_generator(xdata, ydata, i))
+    l = []
+    for i in range(0,24):
+        l.append(lg.lagrange_generator(xdata, ydata, i))
 
-	return l
+    return l
 
 
 
 
 data = main()
 
-print data
+
 def secondary():
-	"""
-	The Point of this is to 
+    """
+	The Point of this is to
 
 	"""
 
-	v = [ 5,  6,  7,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 23]
-	xhat = [i for i in range(7,24)]
-	#This data 
-	prelg = [8901482,  898952, 1107098,  554078,  365004,  427394,  406668,
-        525004,  477256,  571558, 1107482,  909442,  784612,  696754,
-       3112642, 3258892] 
-	yhat = []
-	
-	for r in xhat:
-		yhat.append(lg.lagrange_generator(v, prelg, r))
-	
-	return [np.array(xhat), np.array(yhat)]
+    v = [5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 23]
+    xhat = [i for i in range(7, 24)]
+	#This data
+    prelg = [8901482, 898952, 1107098, 554078, 365004, 427394, 406668,
+             525004, 477256, 571558, 1107482, 909442, 784612, 696754,
+             3112642, 3258892]
+    yhat = []
+
+    for r in xhat:
+        yhat.append(lg.lagrange_generator(v, prelg, r))
+
+    return [np.array(xhat), np.array(yhat)]
 
 
 
@@ -174,8 +176,5 @@ main()
 #plt.axes = data['data_all'].plot(kind='line')
 plt.show()
 
-	
+
 #os.system("prediction.py")
-
-
-
